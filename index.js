8b0b1c028328c16bd6523f4ad803269f4d427c58
Ellementul <ananev.ivan.ur@ya.ru>
2024-02-14 12:33:24 +0500

    Resize and compute cells

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 class Grid {
   constructor(baseLength) {
-    baseLength ||= 20
-    console.log(baseLength)
+    this.baseLength = baseLength || 20
+    this.cell = {
+      height: 0,
+      width: 0
+    }
+    this.computeCells()
+
+    window.addEventListener("resize", () => {
+      this.computeCells()
+      console.log(this.cell)
+    })
+    console.log(this.cell)
+  }
+
+  computeCells() {
+    const baseSide = window.screen.height < window.screen.width ? "height" : "width"
+    const otherSide = window.screen.height < window.screen.width ? "width" : "height"
+ 
+    this.cell[baseSide] = window.screen[baseSide] / this.baseLength
+ 
+    this.cell[otherSide] = window.screen[otherSide] / Math.floor(window.screen[otherSide] / this.cell[baseSide])
   }
 }
 
