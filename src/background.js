01a911c29b52a4d6323dafed026cc77e464118a5
ellementul <ivanzamkov2014@gmail.com>
2024-07-04 12:58:58 +0500

    Clear

--- src/background.js
+++ src/background.js
@@ -58,22 +58,6 @@ export function TillingBackgroundMixin() {
         }
     }
 }
-
- // update() {
-            //     if(this.minSizes.height > this.height)
-            //         this.height = this.minSizes.height
-
-            //     if(this.minSizes.width > this.width)
-            //         this.width = this.minSizes.width
-
-            //     if(this.tileMap)
-            //         this.updateTilling()
-
-            //     if(this.graph)
-            //         this.graph.clear()
-            //             .rect(0, 0, this.width, this.height)
-            //             .fill(this.color)
-            // },
             
             // updateTilling() {
             //     this.tileMap.clear()
@@ -85,38 +69,7 @@ export function TillingBackgroundMixin() {
             //             }
             //         }
             //     }
-            //     if(this.tileMod == TWO_TILE) {
-            //         for (let y = 0; y < this.height; y += this.tileSize) {
-            //             for (let x = 0; x < this.width; x += this.tileSize) {
-            //                 const index = (x + y) % 2
-            //                 const texture = this.textures[index]
-            //                 this.tileMap.tile(texture, x, y)
-            //             }
-            //         }
-            //     }
             // }
-
-
-            // setBackgroundDebug() {
-        //     this.clearBackground()
-        //     this.background.tileMap = new CompositeTilemap
-        //     this.addChild(this.background.tileMap)
-
-        //     this.background.tileMod = TWO_TILE
-        //     this.background.textures = [Texture.WHITE, Texture.EMPTY]
-        //     this.background.tileSize = this.background.textures[0].width
-        //     this.updateSizes()
-        // },
-
-        // setBackgroundColor(color) {
-        //     this.clearBackground()
-
-        //     this.background.color = color || this.background.color
-
-        //     this.background.graph = new Graphics
-        //     this.addChild(this.background.graph)
-        //     this.updateSizes()
-        // },
 
         // setBackgroundTiles(texture) {
         //     this.clearBackground()
@@ -126,42 +79,4 @@ export function TillingBackgroundMixin() {
         //     this.background.textures = [texture]
         //     this.background.tileSize = texture.width > texture.height ? texture.height : texture.width
         //     this.updateSizes()
-        // },
-
-        // clearBackground() {
-        //     if(this.background.tileMap) {
-        //         this.background.tileMap.removeFromParent()
-        //         this.background.tileMap.destroy()
-        //     }
-        //     if(this.background.graph) {
-        //         this.background.graph.removeFromParent()
-        //         this.background.graph.destroy()
-        //     }
-        // },
-
-        // setBackgroundSizes(width, height, minWidth, minHeight) {
-            
-        //     minWidth ||= this.background.minSizes.width
-        //     minHeight ||= this.background.minSizes.height
-        //     this.background.minSizes = {
-        //         width: minWidth,
-        //         height: minHeight
-        //     }
-
-        //     this.background.width = Math.max(width, this.background.minSizes.width)
-        //     this.background.height = Math.max(height, this.background.minSizes.height)
-
-        //     this.updateBackground()
-        // },
-
-        // updateBackground() {
-        //     if(this.background.tileMap) {
-        //         this.background.tileMap.scale = { 
-        //             x: this.screenWidth / this.background.width, 
-        //             y: this.screenHeight / this.background.height
-        //         }
-        //     }
-            
-            
-        //     this.background.update()
         // }
