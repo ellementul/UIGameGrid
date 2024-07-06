1cfd65d33cf21574905b60cf33f7e099bdffa810
ellementul <ivanzamkov2014@gmail.com>
2024-07-06 12:23:18 +0500

    Fixed export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { Grid } from "./grid.js"
 import { Panel } from "./panel.js"
 import { TillingBackgroundMixin } from "../src/background-mixin.js"
+import { UIMemberFactory } from "./ui-member.js"
 
 export {
     Grid,
     Panel,
-    TillingBackgroundMixin
+    TillingBackgroundMixin,
+    UIMemberFactory
 }
